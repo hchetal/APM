c70f3f44f968c6fcd7042472581bb4bf1795a838
Himanshu Chetal <himanshu.chetal@outlook.com>
2018-04-12 23:29:13 +0800

    Adding filter logic

--- src/app/product/product-list.component.ts
+++ src/app/product/product-list.component.ts
@@ -8,6 +8,7 @@ import { IProduct } from "./product";
 })
 export class ProductListComponent implements OnInit {
 
+    
     ngOnInit(): void {
         console.log("This is ngOnInit function");
         
@@ -16,7 +17,28 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage :boolean = false;
-    listFilter:string ="Cart";
+    _listFilter:string;
+
+    constructor(){
+        this.filteredProducts = this.products;
+        this.listFilter="Cart";
+    }
+
+    get listFilter():string{
+        return this._listFilter;
+    }
+
+    set listFilter(value:string) {
+        this._listFilter=value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter):this.products;
+    }
+
+    performFilter(filterBy:string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+      return this.products.filter((product:IProduct)=>product.productName.toLocaleLowerCase().indexOf(filterBy)!= -1);
+    }
+
+    filteredProducts :IProduct[];
     products: IProduct[] = [
         {
             "productId": 1,
