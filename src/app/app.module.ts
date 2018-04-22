945ea9f7e94890b9a5ad4f56cfc028af109b2dbc
Himanshu Chetal <himanshu.chetal@outlook.com>
2018-04-22 22:54:46 +0800

    Creating new component through CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { ProductListComponent } from './product/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star-component';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductDetailComponent } from './product/product-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent,ProductListComponent,ConvertToSpacesPipe, StarComponent
+    AppComponent,ProductListComponent,ConvertToSpacesPipe, StarComponent, ProductDetailComponent
   ],
   imports: [
     BrowserModule,
